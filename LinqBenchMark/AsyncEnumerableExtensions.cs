fe6f46ee149033247c41eedd9f7fbe46548e2bce
türkay ürkmez <turkay.urkmez@outlook.com>
2025-03-01 10:52:26 +0300

    Change extension to commit

--- LinqBenchMark/AsyncEnumerableExtensions.cs
+++ LinqBenchMark/AsyncEnumerableExtensions.cs
@@ -3,29 +3,29 @@
 namespace LinqBenchMark
 {
     
-    public static class AsyncEnumerableExtensions
-    {
-        // IAsyncEnumerable<T> için bir tampon oluşturur ve belirli bir sayıda öğeyi gruplar
-        public static async IAsyncEnumerable<T> BufferAsync<T>(this IAsyncEnumerable<T> source, int count)
-        {
-            var buffer = new List<T>(count);
-            await foreach (var item in source)
-            {
-                buffer.Add(item);
-                if (buffer.Count == count)
-                {
-                    foreach (var i in buffer)
-                    {
-                        yield return i;
-                    }
-                    buffer.Clear();
-                }
-            }
-            foreach (var i in buffer)
-            {
-                yield return i;
-            }
+    //public static class AsyncEnumerableExtensions
+    //{
+    //    // IAsyncEnumerable<T> için bir tampon oluşturur ve belirli bir sayıda öğeyi gruplar
+    //    public static async IAsyncEnumerable<T> BufferAsync<T>(this IAsyncEnumerable<T> source, int count)
+    //    {
+    //        var buffer = new List<T>(count);
+    //        await foreach (var item in source)
+    //        {
+    //            buffer.Add(item);
+    //            if (buffer.Count == count)
+    //            {
+    //                foreach (var i in buffer)
+    //                {
+    //                    yield return i;
+    //                }
+    //                buffer.Clear();
+    //            }
+    //        }
+    //        foreach (var i in buffer)
+    //        {
+    //            yield return i;
+    //        }
 
-        }
-    }
+    //    }
+    //}
 }
